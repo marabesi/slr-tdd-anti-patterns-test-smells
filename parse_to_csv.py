c4f576ccee48dc893190a61ad6dba00b8eec93aa
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-04-19 10:20:04 +0200

    chore: handling extraction for google scholar

--- parse_to_csv.py
+++ parse_to_csv.py
@@ -1,27 +1,34 @@
 import json
 import csv
+import pandas as pd
 
 from os import listdir
 
-files = listdir("./results")
+file_path = "raw-result-from-google-scholar"
+filename = "database/scholar.csv"
 
+files = listdir("./" + file_path)
 list_of_files = []
+
 for file in files:
-    with open("./results/{}".format(file), "r") as jsonFile:
+    with open("./{}/{}".format(file_path,file), "r") as jsonFile:
         parsed = json.load(jsonFile)
         list_of_files.append(parsed)
 
-data_file = open('parsed-google-scholar-to-csv.csv', 'w')
+data_file = open('{}'.format(filename), 'w')
 csv_writer = csv.writer(data_file)
 
 count = 0
 total = 0
+
 for data in list_of_files:
+    df = pd.json_normalize(data, sep='_')
+    row = df.to_dict(orient='records')[0]
     if count == 0:
-        header = data.keys()
+        header = row.keys()
         csv_writer.writerow(header)
         count += 1
-    csv_writer.writerow(data.values())
+    csv_writer.writerow(row.values())
     total += 1
  
 data_file.close()
