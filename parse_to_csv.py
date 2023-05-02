6098cd9dc908b3fd67c7c09ff6536dc65c062f50
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-05-02 21:38:18 +0200

    fix: adds id to scholar column

--- parse_to_csv.py
+++ parse_to_csv.py
@@ -10,21 +10,21 @@ filename = "database/scholar.csv"
 files = listdir("./" + file_path)
 list_of_files = []
 
+total = 1
 for file in files:
     with open("./{}/{}".format(file_path,file), "r") as jsonFile:
-        parsed = json.load(jsonFile)
+        base = dict({ 'id': total })
+        parsed = base | json.load(jsonFile)
         list_of_files.append(parsed)
+        total += 1
 
 data_file = open('{}'.format(filename), 'w')
 csv_writer = csv.writer(data_file)
 
 count = 0
-total = 0
-
 for data in list_of_files:
     df = pd.json_normalize(data, sep='_')
     row = df.to_dict(orient='records')[0]
-    row['id'] = total
 
     if count == 0:
         header = row.keys()
@@ -32,8 +32,7 @@ for data in list_of_files:
         count += 1
 
     csv_writer.writerow(row.values())
-    total += 1
- 
+
 data_file.close()
 
-print("{} row(s)".format(total))
+print("{} row(s)".format(total -1 ))
