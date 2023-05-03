e9f0ada2e400d41aefc5ceb0555a5a1fa508c213
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-05-03 21:46:01 +0200

    chore: script to merge all data

---
+++ merge.py
@@ -0,0 +1,73 @@
+import csv
+
+
+file_list = []
+
+def build_record(title, authors, year, source, doi, url_for_publication):
+    return {
+        'title': title,
+        'authors': authors,
+        'year': year,
+        'source': source,
+        'doi': doi,
+        'url_of_publication': url_for_publication
+    }
+
+with open("database/scholar.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        file_list.append(
+            build_record(
+                row['bib_title'],
+                row['bib_author'],
+                row['bib_pub_year'],
+                row['bib_venue'],
+                row['eprint_url'],
+                row['pub_url'],
+            )
+        )
+
+with open("database/scopus.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        file_list.append(
+            build_record(
+                row['Title'],
+                row['\ufeffAuthors'],
+                row['Year'],
+                row['Source'],
+                row['DOI'],
+                row['Link'],
+            )
+        )
+
+with open("database/wos.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        file_list.append(
+            build_record(
+                row['Article Title'],
+                row['Author Full Names'],
+                row['Publication Year'],
+                row['Source Title'],
+                row['DOI'],
+                "https://doi.org/" + row['DOI'],
+            )
+        )
+
+filename = "database/merged.csv"
+data_file = open('{}'.format(filename), 'w')
+csv_writer = csv.writer(data_file)
+
+count = 0
+for data in file_list:
+    if count == 0:
+        header = data.keys()
+        csv_writer.writerow(header)
+        count += 1
+
+    csv_writer.writerow(data.values())
+
+data_file.close()
+
+print("{} articles merged".format(len(filename)))
